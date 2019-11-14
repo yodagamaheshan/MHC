aa68808111806b8bdb0c1ef22e9539c248ae0c57
Heshan Yodagama <yodagamaheshan@gmail.com>
2019-11-14 09:54:28 +0530

    Initial Commit

---
+++ HMC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HMC
+//
+//  Created by Heshan Yodagama on 11/14/19.
+//  Copyright © 2019 Heshan Yodagama. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
