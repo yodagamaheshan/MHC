5c88a01d1643ae6617e1c35fffc3e2b0ba770263
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-02-13 20:41:44 +0530

    add file for voteForHikeRow

---
+++ HMC/SplashView/HomeView/VoteForHikeRow/VoteForHikeRow.swift
@@ -0,0 +1,21 @@
+//
+//  VoteForHikeRow.swift
+//  HMC
+//
+//  Created by Heshan Yodagama on 2/13/20.
+//  Copyright © 2020 Heshan Yodagama. All rights reserved.
+//
+
+import SwiftUI
+
+struct VoteForHikeRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VoteForHikeRow_Previews: PreviewProvider {
+    static var previews: some View {
+        VoteForHikeRow()
+    }
+}
