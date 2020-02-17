e9973e3a684bf0a1036c9ed2ca2eaaed339c2b29
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-02-17 21:57:12 +0530

    add form row file

---
+++ HMC/SplashView/HomeView/FormRow/FormRow.swift
@@ -0,0 +1,21 @@
+//
+//  FormRow.swift
+//  HMC
+//
+//  Created by Heshan Yodagama on 2/17/20.
+//  Copyright © 2020 Heshan Yodagama. All rights reserved.
+//
+
+import SwiftUI
+
+struct FormRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FormRow_Previews: PreviewProvider {
+    static var previews: some View {
+        FormRow()
+    }
+}
